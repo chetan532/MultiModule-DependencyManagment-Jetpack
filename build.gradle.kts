d62aaf0e75fc1fd59484b865f75f54bcf4034950
Chetan Vaghela <chetanvaghela457@gmail.com>
2023-10-16 21:13:33 +0530

    Initial Commit

---
+++ build.gradle.kts
@@ -0,0 +1,14 @@
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.10")
+    }
+
+    dependencies {
+
+        classpath(Dependencies.hiltAgp)
+    }
+}
